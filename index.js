fa63e05d3cfe5054e6c20853f8e9ae24c38a0fa8
Harry Alvin Waidan Kefas <harry.kambey@gmail.com>
2018-06-06 19:44:56 +0700

    refine conversion API

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var MongoClient = require('mongodb').MongoClient
+var mongo = require('mongodb')
+var _ = require('lodash')
 
 var app = express()
+var bodyParser = require('body-parser')
   
 app.use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
@@ -52,20 +58,28 @@ const convertCSV = async (req, res) => {
       sent_data += read_data
     })
   
-    readstream.on('end', () => {
+    readstream.on('end', async () => {
       var converted_data = JSON.parse(sent_data)
 
       // save data
+      var connection_string = 'mongodb://127.0.0.1/linkeddata'
+      var db = await MongoClient.connect(connection_string)
+      var collection = db.db('linkeddata').collection('data')
 
+      var insert_status = await collection.insert(converted_data)
+      
       // get inserted ids
-
-      // generate URIs
+      var inserted_links = []
+      for (let i = 0; i < insert_status['insertedCount']; i++) {
+        // generate URIs
+        inserted_links.push('http://localhost:5000/dataset/linked_data/' + insert_status['insertedIds']['' + i])
+      }
 
       // add context (data vocab)
       
       // send response, convert data done.
       the_response = {
-        data: converted_data
+        data: inserted_links
       }
       res.send(the_response)
     })
@@ -74,6 +88,13 @@ const convertCSV = async (req, res) => {
 
   
   
+}
+
+var to = (promise) => {
+  return promise.then((data) => {
+    return [null, data];
+  })
+  .catch((err) => [err])
 }
 
 app.post('/convert/*', upload.single('data'), (req, res) => {
@@ -93,7 +114,78 @@ app.post('/convert/*', upload.single('data'), (req, res) => {
 
 })
 
+var handleGetData = async (req, res) => {
+  var id_entity = req.params.id_entity
+
+  var connection_string = 'mongodb://127.0.0.1/linkeddata'
+  var db = await MongoClient.connect(connection_string)
+  var collection = db.db('linkeddata').collection('data')
+
+  var err
+  var fetch_result = {}
+  [err, fetch_result] = await collection.findOne({ _id: new mongo.ObjectID(id_entity) }) === null 
+    ? {} 
+    : await collection.findOne({ _id: new mongo.ObjectID(id_entity) })
+
+  if (err) {
+    res.send({error: err})
+  }
+  res.send(fetch_result)
+}
+
+app.get('/dataset/linked_data/:id_entity', (req, res) => 
+  handleGetData(req, res).then(function() {
+    console.log('done')
+  })
+)
+
+var handleLinkingData = async (req, res) => {
+
+  var body = req.body
+
+  console.log(req.body)
+
+  var subj = body['subj']
+  var obj = body['obj']
+  var pred_dest = {}
+  pred_dest[body['pred']] = 'http://localhost:5000/dataset/linked_data/' + obj
+  var pred_src = {}
+  pred_src[body['pred']] = 'http://localhost:5000/dataset/linked_data/' + subj
+
+  var connection_string = 'mongodb://127.0.0.1/linkeddata'
+  var db = await MongoClient.connect(connection_string)
+  var collection = db.db('linkeddata').collection('data')
+
+  var update_status_dest = await collection.update(
+    { _id: new mongo.ObjectID(subj) },
+    {
+      '$set': pred_dest
+    }
+  )
+  var update_status_src = await collection.update(
+    { _id: new mongo.ObjectID(obj) },
+    {
+      '$set': pred_src
+    }
+  )
+
+  console.log(update_status_dest)
+  console.log(update_status_src)
+
+  var new_data = []
+  var dest_new_data = await collection.findOne({ _id: new mongo.ObjectID(subj) })
+  var src_new_data = await collection.findOne({ _id: new mongo.ObjectID(obj) })
+  new_data.push(dest_new_data)
+  new_data.push(src_new_data)
+
+  res.send(new_data)
+}
+
 app.post('/linking', (req, res) => {
   
   // linking entities
+
+  handleLinkingData(req, res).then(function() {
+    console.log('done')
+  })
 })
